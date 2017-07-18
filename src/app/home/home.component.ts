6caa3d29536eeb9c3475ad0307fa6d69227ec6a0
gsse7en <latyshev.fedor@gmail.com>
2017-07-18 16:20:09 +0300

    stripped unused Common module import from rich, genious, superpowers components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { PersonsService } from '../persons.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   templateUrl: 'home.component.html'
--- src/app/superpowers/superpowers.component.ts
+++ src/app/superpowers/superpowers.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PersonsService } from '../persons.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   templateUrl: 'superpowers.component.html'
